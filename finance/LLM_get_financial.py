f6d160fed18b57cdc9840747d1f6d77688ec6232
Noa <noabendet@post.runi.ac.il>
2025-01-23 18:52:48 +0200

    Add: qualitative functions to extract business info and competitors

--- finance/LLM_get_financial.py
+++ finance/LLM_get_financial.py
@@ -1,9 +1,12 @@
 """
     LLM_get_financial.py - Functionsfor the Analyst agents
 """
-from finance.balance_sheet_helpers import get_assets, get_inventory, get_liabilities
+import os
+from dotenv import load_dotenv
+import requests
+from balance_sheet_helpers import get_assets, get_inventory, get_liabilities
 
-def quick_ratio(ticker: str, year: str) -> float|None:
+def quick_ratio(ticker: str, year: str):
     """
     Quick Ratio = (Current Assets - Inventories) / Current Liabilities
     """
@@ -15,4 +18,41 @@ def quick_ratio(ticker: str, year: str) -> float|None:
         return None
         
     return (current_assets - inventory) / current_liabilities if current_liabilities != 0 else 0
+
+
+def get_related_companies(ticker: str, n = 1) -> list:
+    """
+    Fetch up to n related tickers for the given ticker from Polygon.io.
+
+    args: ticker: The stock symbol for which related tickers are requested (e.g., "AAPL").
+            n: The maximum number of related tickers to return.
+            api_key: Your Polygon.io API key.
+
+    return: A list of related ticker symbols.
+    """
+    load_dotenv()
+    api_key_polygon = os.getenv('POLYGON_API_KEY')
+    url = f"https://api.polygon.io/v1/related-companies/{ticker}"
+    params = {
+        "apiKey": api_key_polygon
+    }
+
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Polygon.io API request failed with status code "
+            f"{response.status_code} and error: {response.text}"
+        )
+
+    data = response.json()
+    print(data)
+
+    top_n_competitors = []
+    related_tickers = data.get("results", [])
+    for ticker in related_tickers:
+        top_n_competitors.append(ticker["ticker"])
+        if len(top_n_competitors) >= n:
+            break
+
+    return top_n_competitors
 
---
+++ finance/LLM_get_qualitative.py
@@ -0,0 +1,48 @@
+"""
+    LLM_get_qualitive.py - Functions for the qualitive Analyst agents
+"""
+import yfinance as yf
+import requests
+import os
+from dotenv import load_dotenv
+
+def get_company_data(ticker):
+    """
+    Fetches API company data from Polygon.io API.
+
+    Args:
+        ticker (str): The stock ticker symbol.
+
+    Returns:
+        dict: A dictionary containing news articles related to the company.
+
+    """
+    load_dotenv()
+    api_key_polygon = os.getenv('POLYGON_API_KEY')
+    news_url = f"https://api.polygon.io/v2/reference/news?ticker={ticker}&limit=6&apiKey={api_key_polygon}"
+    news = requests.get(news_url).json().get('results')
+    articles_info = {}
+    for index, news_dict in enumerate(news):
+        title = news_dict['title']
+        description = news_dict['description']
+        insights = news_dict['insights']
+        current_article = {'Title' : title, 'description': description, 'insights': insights}
+        articles_info[index + 1] = current_article
+    return articles_info
+
+
+def extract_businees_info(company_ticker):
+   """
+   Extract strategic elements from company info dictionary
+
+    Args: 
+        company_ticker (str): The stock ticker symbol.
+
+    Returns:
+        dict: A dictionary containing a business summary of the company.   
+   """
+   company_info = yf.Ticker(company_ticker).info
+   business_info= {
+       'longBusinessSummary': company_info.get('longBusinessSummary')
+    }
+   return business_info
