1e95be82b7e56b49dc0b273bdd919e6d0c430f06
Noa <noabendet@post.runi.ac.il>
2025-01-23 18:59:08 +0200

    Add: app constants for init group chat

---
+++ app_constants.py
@@ -0,0 +1,13 @@
+"""
+    app_constants.py - constants file for init the group chat
+"""
+
+BUDGET = 100000  
+
+TICKER_STOCKS = [
+    "AAPL",  # Apple Inc.
+    "LULU",  # Lululemon Athletica Inc.
+    "JNJ",   # Johnson & Johnson
+    "NFLX",  # Netflix, Inc.
+    "TSLA"    # Exxon Mobil Corporation
+]
