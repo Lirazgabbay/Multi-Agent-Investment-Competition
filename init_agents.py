f5f55a4c38b84bcc17937411655c3be35121d6a1
Noa <noabendet@post.runi.ac.il>
2025-01-25 19:11:16 +0200

    Fix: LLM messages formatting

--- init_agents.py
+++ init_agents.py
@@ -1,56 +1,91 @@
 import autogen
-from config.agent_config import COMPETATIVE_MARGIN_MULTIPLIER_CONFIG, HISTORICAL_MARGIN_MULTIPLIER_CONFIG, MODERATOR_CONFIG, QUALITATIVE_CONFIG, SOCIAL_ANALYST_CONFIG, LIQUIDITY_CONFIG
+from config.available_functions import LIQUIDITY_AVAILABLE_FUNCTIONS, MARGIN_MULTIPLIER_AVAILABLE_FUNCTIONS, QUALITATIVE_AVAILABLE_FUNCTIONS
+from config.config_list_LLM import CONFIG_LLM_GPT4
 from config.system_messages import SYSTEM_MSG_COMPETATIVE_MARGIN_MULTIPLIER_CONFIG, SYSTEM_MSG_HISTORICAL_MARGIN_MULTIPLIER_CONFIG, SYSTEM_MSG_LIQUIDITY_CONFIG, SYSTEM_MSG_QUALITATIVE_CONFIG
-# TODO: refactor all base on the agents  
+from finance.LLM_get_financial import quick_ratio
+from finance.LLM_get_qualitative import extract_business_info, get_company_data
+from finance.profit_margin import gross_profit_margin, net_profit_margin, operational_profit_margin
+from finance.profit_multipliers import price_earnings_to_growth_ratio, price_sales_ratio, price_to_EBIT_ratio, price_to_book_value_ratio, price_to_earnings_ratio
 
 class InitAgents():
     def __init__(self):        
-        # Create the agents
-
-        # is_termination_msg: Optional[Callable[[Dict], bool]] = None,
-        # max_consecutive_auto_reply: Optional[int] = None,
-
         self.liquidity_agent = autogen.AssistantAgent(
-            name=LIQUIDITY_CONFIG["name"],
-            llm_config=LIQUIDITY_CONFIG["llm_config"],
+            name="Liquidity_Analyst",
+            llm_config={
+                "config_list": CONFIG_LLM_GPT4,
+                "temperature": 0.4,
+                "functions": LIQUIDITY_AVAILABLE_FUNCTIONS
+            },
             system_message=SYSTEM_MSG_LIQUIDITY_CONFIG,
-            human_input_mode="NEVER"
+            human_input_mode="NEVER",
+            function_map={
+                "quick_ratio": quick_ratio
+            }
         )
 
         self.historical_margin_multiplier_analyst = autogen.AssistantAgent(
-            name=HISTORICAL_MARGIN_MULTIPLIER_CONFIG["name"],
-            llm_config=SOCIAL_ANALYST_CONFIG["llm_config"],
+            name="Historical_Margin_Multiplier_Analyst",
+            llm_config={
+                "config_list": CONFIG_LLM_GPT4,
+                "temperature": 0.4,
+                "functions": MARGIN_MULTIPLIER_AVAILABLE_FUNCTIONS
+            },
             system_message=SYSTEM_MSG_HISTORICAL_MARGIN_MULTIPLIER_CONFIG,
-            human_input_mode="NEVER"
+            human_input_mode="NEVER",
+            function_map={
+                "gross_profit_margin": gross_profit_margin,
+                "operational_profit_margin": operational_profit_margin,
+                "net_profit_margin": net_profit_margin,
+                "price_sales_ratio": price_sales_ratio,
+                "price_to_EBIT_ratio": price_to_EBIT_ratio,
+                "price_to_book_value_ratio": price_to_book_value_ratio,
+                "price_to_earnings_ratio": price_to_earnings_ratio,
+                "price_earnings_to_growth_ratio": price_earnings_to_growth_ratio
+            }
         )
 
         self.competative_margin_multiplier_analyst = autogen.AssistantAgent(
-            name=COMPETATIVE_MARGIN_MULTIPLIER_CONFIG["name"],
-            llm_config=COMPETATIVE_MARGIN_MULTIPLIER_CONFIG["llm_config"],
+            name="Competative_Margin_Multiplier_Analyst",
+            llm_config={
+                "config_list": CONFIG_LLM_GPT4,
+                "temperature": 0.4,
+                "functions": MARGIN_MULTIPLIER_AVAILABLE_FUNCTIONS
+            },
             system_message=SYSTEM_MSG_COMPETATIVE_MARGIN_MULTIPLIER_CONFIG,
-            human_input_mode="NEVER"
+            human_input_mode="NEVER",
+            function_map={
+                "gross_profit_margin": gross_profit_margin,
+                "operational_profit_margin": operational_profit_margin,
+                "net_profit_margin": net_profit_margin,
+                "price_sales_ratio": price_sales_ratio,
+                "price_to_EBIT_ratio": price_to_EBIT_ratio,
+                "price_to_book_value_ratio": price_to_book_value_ratio,
+                "price_to_earnings_ratio": price_to_earnings_ratio,
+                "price_earnings_to_growth_ratio": price_earnings_to_growth_ratio
+            }
         )
 
-        self.quantitative_analyst = autogen.AssistantAgent(
-            name=QUALITATIVE_CONFIG["name"],
-            llm_config=QUALITATIVE_CONFIG["llm_config"],
+
+        self.qualitative_analyst = autogen.AssistantAgent(
+            name="Qualitative_Analyst",
+            llm_config={
+                "config_list": CONFIG_LLM_GPT4,
+                "temperature": 0.4,
+                "functions" : QUALITATIVE_AVAILABLE_FUNCTIONS
+            },
             system_message=SYSTEM_MSG_QUALITATIVE_CONFIG,
-            human_input_mode="NEVER"
+            human_input_mode="NEVER",
+            function_map={
+                "get_company_data": get_company_data,
+                "extract_business_info": extract_business_info
+            }
         )
 
-
-
-        # self.moderator = autogen.AssistantAgent(
-        #     name="Investment_Moderator",
-        #     system_message=MODERATOR_CONFIG["system_message"],
-        #     llm_config=MODERATOR_CONFIG["llm_config"]
-        # )
-
-        # # Create the user proxy
-        # self.user_proxy = autogen.UserProxyAgent(
-        #     name="user_proxy",
-        #     human_input_mode="NEVER",
-        #     max_consecutive_auto_reply=10,
-        #     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("DISCUSSION_END"),
-        #     code_execution_config=False,
-        # )
+        self.user_proxy = autogen.UserProxyAgent(
+            name="user_proxy",
+            human_input_mode="NEVER",
+            max_consecutive_auto_reply=10,
+            is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("DISCUSSION_END"),
+            code_execution_config=False,
+            system_message="The agent that initialize the conversation",
+        )
