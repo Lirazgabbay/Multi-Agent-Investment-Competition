0c8507b8520d7094c5d8f116eb77b953886476c5
Liraz gabbay <liraz.gabby@runi.ac.il>
2025-02-26 15:23:20 +0200

    Add: functions and functions map to each agent

--- init_agents.py
+++ init_agents.py
@@ -1,42 +1,252 @@
 import autogen
-from config.available_functions import LIQUIDITY_AVAILABLE_FUNCTIONS, MARGIN_MULTIPLIER_AVAILABLE_FUNCTIONS, QUALITATIVE_AVAILABLE_FUNCTIONS
 from config.config_list_LLM import CONFIG_LLM_GPT4
 from config.system_messages import SYS_MSG_MANAGER_CONFIG, SYSTEM_MSG_COMPETATIVE_MARGIN_MULTIPLIER_CONFIG, SYSTEM_MSG_HISTORICAL_MARGIN_MULTIPLIER_CONFIG, SYSTEM_MSG_LIQUIDITY_CONFIG, SYSTEM_MSG_QUALITATIVE_CONFIG
-from finance.LLM_get_financial import quick_ratio
+from finance.LLM_get_financial import get_related_companies, quick_ratio
 from finance.LLM_get_qualitative import extract_business_info, get_company_data
-from finance.profit_margin import gross_profit_margin, net_profit_margin, operational_profit_margin
-from finance.profit_multipliers import price_earnings_to_growth_ratio, price_sales_ratio, price_to_EBIT_ratio, price_to_book_value_ratio, price_to_earnings_ratio
+from finance.profit_margin import calculate_profit_margins
+from finance.profit_multipliers import price_to_EBIT_ratio, ratios
 
 class InitAgents():
     def __init__(self):        
         self.liquidity_agent = autogen.AssistantAgent(
             name="Liquidity_Analyst",
-            llm_config={
+            llm_config = {
                 "config_list": CONFIG_LLM_GPT4,
                 "temperature": 0.4,
+                "functions": [
+                    {
+                        "name": "quick_ratio",
+                        "description": "Calculates the Quick Ratio for a given stock symbol and year.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "symbol": {
+                                    "type": "string",
+                                    "description": "Stock ticker symbol for the company.",
+                                },
+                                "year": {
+                                    "type": "integer",
+                                    "description": "Year for which the quick ratio is calculated.",
+                                }
+                            },
+                            "required": ["symbol", "year"],
+                        },
+                    },
+                ],
             },
             system_message=SYSTEM_MSG_LIQUIDITY_CONFIG,
             human_input_mode="NEVER",
+            function_map={"quick_ratio": quick_ratio}
         )
+        
+        # self.historical_margin_multiplier_analyst = autogen.AssistantAgent(
+        #     name="Historical_Margin_Multiplier_Analyst",
+        #     llm_config={
+        #         "config_list": CONFIG_LLM_GPT4,
+        #         "temperature": 0.4,
+        #         "functions": [
+        #             {
+        #                 "name": "price_to_EBIT_ratio",
+        #                 "description": "Calculate the Price/EBIT ratio for a given stock symbol and year.",
+        #                 "parameters": {
+        #                     "type": "object",
+        #                     "properties": {
+        #                         "symbol": {
+        #                             "type": "string",
+        #                             "description": "Stock symbol for the company.",
+        #                         },
+        #                         "year": {
+        #                             "type": "integer",
+        #                             "description": "Year for which the ratio is calculated."
+        #                         }
+        #                     },
+        #                     "required": ["symbol", "year"]
+        #                 }
+        #             }
+        #         ],
+        #     },
+        #     system_message=SYSTEM_MSG_HISTORICAL_MARGIN_MULTIPLIER_CONFIG,
+        #     human_input_mode="NEVER",
+        #     function_map={"price_to_EBIT_ratio":price_to_EBIT_ratio}
+        # )
 
         self.historical_margin_multiplier_analyst = autogen.AssistantAgent(
             name="Historical_Margin_Multiplier_Analyst",
             llm_config={
                 "config_list": CONFIG_LLM_GPT4,
                 "temperature": 0.4,
+                "functions": [
+                    {
+                        "name": "price_to_EBIT_ratio",
+                        "description": "Calculate the Price/EBIT ratio for a given stock symbol.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "symbol": {
+                                    "type": "string",
+                                    "description": "Stock symbol for the company.",
+                                },
+                                "year": {
+                                    "type": "integer",
+                                    "description": "Year for which the ratio is calculated."
+                                }
+                            },
+                            "required": ["symbol", "year"]
+                        }
+                    },
+                    {
+                        "name": "ratios",
+                        "description": "Calculate the ratios for a given stock symbol.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "symbol": {
+                                    "type": "string",
+                                    "description": "Stock symbol for the company.",
+                                },
+                                "year": {
+                                    "type": "integer",
+                                    "description": "Year for which the ratio is calculated."
+                                }
+                            },
+                            "required": ["symbol", "year"]
+                        }
+                    },
+                    {
+                        "name": "calculate_profit_margins",
+                        "description": "Calculate profit margins for a given stock symbol.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "symbol": {
+                                    "type": "string",
+                                    "description": "Stock symbol for the company.",
+                                },
+                                "year": {
+                                    "type": "integer",
+                                    "description": "Year for which the ratio is calculated."
+                                }
+                            },
+                            "required": ["symbol", "year"]
+                        }
+                    }
+                ],
             },
             system_message=SYSTEM_MSG_HISTORICAL_MARGIN_MULTIPLIER_CONFIG,
             human_input_mode="NEVER",
+            function_map={"price_to_EBIT_ratio":price_to_EBIT_ratio,"ratios": ratios, "calculate_profit_margins": calculate_profit_margins}
         )
+
+        # self.historical_margin_multiplier_analyst = autogen.AssistantAgent(
+        #     name="Historical_Margin_Multiplier_Analyst",
+        #     llm_config={
+        #         "config_list": CONFIG_LLM_GPT4,
+        #         "temperature": 0.4,
+        #         "functions": [
+        #             {
+        #                 "name": "quick_ratio",
+        #                 "description": "Calculates the Quick Ratio for a given stock symbol and year.",
+        #                 "parameters": {
+        #                     "type": "object",
+        #                     "properties": {
+        #                         "symbol": {
+        #                             "type": "string",
+        #                             "description": "Stock ticker symbol for the company.",
+        #                         },
+        #                         "year": {
+        #                             "type": "integer",
+        #                             "description": "Year for which the quick ratio is calculated.",
+        #                         }
+        #                     },
+        #                     "required": ["symbol", "year"],
+        #                 },
+        #             },
+        #         ],
+        #     },
+        #     system_message=SYSTEM_MSG_HISTORICAL_MARGIN_MULTIPLIER_CONFIG,
+        #     human_input_mode="NEVER",
+        #     function_map={"quick_ratio": quick_ratio}
+        # )
 
         self.competative_margin_multiplier_analyst = autogen.AssistantAgent(
             name="Competative_Margin_Multiplier_Analyst",
             llm_config={
                 "config_list": CONFIG_LLM_GPT4,
                 "temperature": 0.4,
+                "functions": [
+                    {
+                        "name": "get_related_companies",
+                        "description": "Get related companies for a given stock symbol.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "symbol": {
+                                    "type": "string",
+                                    "description": "The symbol of the company to get related companies for"
+                                },
+                            },
+                            "required": ["symbol"]
+                        }
+                    },
+                    {
+                        "name": "price_to_EBIT_ratio",
+                        "description": "Calculate the Price/EBIT ratio for a given stock symbol.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "symbol": {
+                                    "type": "string",
+                                    "description": "Stock symbol for the company.",
+                                },
+                                "year": {
+                                    "type": "integer",
+                                    "description": "Year for which the ratio is calculated."
+                                }
+                            },
+                            "required": ["symbol", "year"]
+                        }
+                    },
+                    {
+                        "name": "ratios",
+                        "description": "Calculate the ratios for a given stock symbol.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "symbol": {
+                                    "type": "string",
+                                    "description": "Stock symbol for the company.",
+                                },
+                                "year": {
+                                    "type": "integer",
+                                    "description": "Year for which the ratio is calculated."
+                                }
+                            },
+                            "required": ["symbol", "year"]
+                        }
+                    },
+                    {
+                        "name": "calculate_profit_margins",
+                        "description": "Calculate profit margins for a given stock symbol.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "symbol": {
+                                    "type": "string",
+                                    "description": "Stock symbol for the company.",
+                                },
+                                "year": {
+                                    "type": "integer",
+                                    "description": "Year for which the ratio is calculated."
+                                }
+                            },
+                            "required": ["symbol", "year"]
+                        }
+                    }
+                ]
             },
             system_message=SYSTEM_MSG_COMPETATIVE_MARGIN_MULTIPLIER_CONFIG,
             human_input_mode="NEVER",
+            function_map={"get_related_companies": get_related_companies, "ratios": ratios, "price_to_EBIT_ratio": price_to_EBIT_ratio,"calculate_profit_margins": calculate_profit_margins}
         )
 
 
@@ -45,9 +255,40 @@ class InitAgents():
             llm_config={
                 "config_list": CONFIG_LLM_GPT4,
                 "temperature": 0.4,
+                "functions": [
+                    {
+                        "name": "extract_business_info",
+                        "description": "Extract strategic elements from company information.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "symbol": {
+                                    "type": "string",
+                                    "description": "The stock symbol of the company."
+                                }
+                            },
+                            "required": ["symbol"]
+                        }
+                    },
+                    {
+                        "name": "get_company_data",
+                        "description": "Fetch news articles related to the company.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "symbol": {
+                                    "type": "string",
+                                    "description": "The stock symbol."
+                                }
+                            },
+                            "required": ["symbol"]
+                        }
+                    }
+                ]
             },
             system_message=SYSTEM_MSG_QUALITATIVE_CONFIG,
-            human_input_mode="NEVER"
+            human_input_mode="NEVER",
+            function_map={"extract_business_info": extract_business_info, "get_company_data": get_company_data}
         )
 
         self.user_proxy = autogen.UserProxyAgent(
