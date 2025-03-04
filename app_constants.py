c440888c601e0454119a68890605be862aaa6f68
Noa <noabendet@post.runi.ac.il>
2025-03-04 11:52:08 +0200

    Add: start_year - limit the search info until the start year of investment

--- app_constants.py
+++ app_constants.py
@@ -2,16 +2,9 @@
     app_constants.py - constants file for init the group chat
 """
 
-BUDGET = 100000  
+BUDGET=100000  
 
 TICKER_STOCKS = [
     "AAPL"  # Apple Inc.
 ]
-
-# TICKER_STOCKS = [
-#     "AAPL",  # Apple Inc.
-#     "LULU",  # Lululemon Athletica Inc.
-#     "JNJ",   # Johnson & Johnson
-#     "NFLX",  # Netflix, Inc.
-#     "TSLA"    # Exxon Mobil Corporation
-# ]
+START_YEAR=2022
